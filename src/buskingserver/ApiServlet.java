3a8c815c6d7e8db55d72f854810ee5c256979e70
sohyeon <thgus118945@naver.com>
2014-06-23 06:54:03 +0900

    6/23

--- src/buskingserver/ApiServlet.java
+++ src/buskingserver/ApiServlet.java
@@ -18,27 +18,79 @@ import com.google.appengine.api.datastore.Query.FilterPredicate;
 
 @SuppressWarnings("serial")
 public class ApiServlet extends HttpServlet {
-	
+
 	@Override
-	public void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
-		
-		resp.setContentType("text/plain; charset=UTF-8");
+	public void doGet(HttpServletRequest req, HttpServletResponse resp)
+			throws IOException {
+
+		resp.setContentType("text/xml; charset=UTF-8");
 		resp.setCharacterEncoding("utf-8");
-		
+
 		DatastoreService datastore = DatastoreServiceFactory.getDatastoreService();
-		
+
 		String str = req.getParameter("search");
-		Filter mFilter = new FilterPredicate("name", FilterOperator.EQUAL, str);
-		Query query = new Query("Perfor");
+		Filter mFilter = null;
+		Query query = null;
+
+		if (req.getParameter("kind").equals("n"))
+		{
+			mFilter = new FilterPredicate("name", FilterOperator.EQUAL, str);
+		} 
+		else if (req.getParameter("kind").equals("d")) 
+		{
+			mFilter = new FilterPredicate("date", FilterOperator.EQUAL, str);
+		}
+
+		query = new Query("Perfor");
 		query.setFilter(mFilter);
 		PreparedQuery pq = datastore.prepare(query);
-		
-		String perfor = "";
-		for(Entity result : pq.asIterable()) {
-			perfor += (String) result.getProperty("pr");
-		}
-		
+
 		PrintWriter out = resp.getWriter();
-		out.println(perfor);
+		String outString = "";
+		/*
+		 * for(Entity result : pq.asIterable()) { perfor += (String)
+		 * result.getProperty("pr"); }
+		 */
+
+		try {
+
+			// Connection conn = DriverManager.getConnection(url, "testuser",
+			// "5555");
+
+			// ResultSet rs = (ResultSet) conn.createStatement().executeQuery(
+			// "SELECT guestName, content, entryID FROM guestbook.entries");
+
+			outString = "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n";
+			outString += "<data>";
+			outString += "<result>1</result>\n";
+			outString += "<documents>\n";
+			//
+			for (Entity result : pq.asIterable())
+			{
+				outString += "<document>\n";
+				outString += "<name>" + result.getProperty("name") + "</name>";
+				outString += "<date>" + result.getProperty("date") + "</date>";
+				outString += "<time>" + result.getProperty("time") + "</time>";
+				outString += "<pr>" + result.getProperty("pr") + "</pr>";
+				outString += "<lat>" + result.getProperty("latitude") + "</lat>";
+				outString += "<lon>" + result.getProperty("longitude") + "</lon>";
+				// outString += "<content>"+new
+				// String(result.getString("content"))+"</content>";
+				// outString += "<id>"+result.getString("entryID")+"</id>";
+				outString += "</document>\n";
+			}
+			outString += "</documents>\n";
+			// conn.close();
+
+		} catch (Exception e) {
+			System.out.println(e.toString());
+			outString = "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n";
+			outString += "<result>2</result>\n";
+			e.printStackTrace();
+		}
+		outString += "</data>";
+		// System.out.println(obj.toString());
+		out.println(outString);
+		// out.println(gson.toJson(books));
 	}
 }
